b4fb5941e5604b65dca4a8e6a4c123324019511a
Jose Roberts <webterpr@gmail.com>
2017-08-09 01:06:51 -0400

    implementing the stocks by symbol

--- api/controllers/stock.controllers.js
+++ api/controllers/stock.controllers.js
@@ -2,5 +2,26 @@ var mongoose = require('mongoose');
 var Stock = mongoose.model('Stock')
 
 module.exports.getStock = function(req, res){
+    var stockId = req.params.stockId
+    console.log('got stock symbol', stockId);
+
+    Stock
+    .find(stockId)
+    .exec(function(err, doc){
+        if(err){
+            console.log('error finding stock')
+            res
+            .status(500)
+            .json(err);
+        }else if (!doc){
+            res
+            .status(404)
+            .json({'message': 'Stock Symbol not found'});
+        }else{
+            res
+            .status(200)
+            .json(doc);
+        }
+    })
 
 };
--- public/angular-app/stocks-display/stocks-display-controller.js
+++ public/angular-app/stocks-display/stocks-display-controller.js
@@ -1,11 +1,12 @@
 angular.module('meannasdaq').controller('StocksController', StocksController);
 
-function StocksController(nasdaqDataFactory){
+function StocksController($route, $routeParams, nasdaqDataFactory){
     var vm = this;
+    var id = $routeParams.Symbol;
     vm.title = 'MEAN Nasdaq App';
     //     $http.get('/api/hotels?count=10').then(function(response){
-    nasdaqDataFactory.nasdaqList().then(function(response){
+    nasdaqDataFactory.stocksDisplay(id).then(function(response){
         console.log(response);
-        vm.hotels = response;
+        vm.stock = response;
     });
 }
