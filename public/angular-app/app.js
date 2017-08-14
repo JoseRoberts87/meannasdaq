ab38e49264af7339409e6422ff103aceea2c78b7
Jose Roberts <webterpr@gmail.com>
2017-08-14 00:20:31 -0400

    made changes view as a single page app

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -1,33 +1,32 @@
 angular.module('meannasdaq', ['ngRoute', 'angular-jwt']).config(config)
 
-function config($httpProvider, $routeProvider,$locationProvider) {
+function config($httpProvider, $routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
     $httpProvider
     $routeProvider
-    .when('/', {
-        templateUrl: 'angular-app/main/main.html',
-        access:{
-            restricted: false
-        }
-    })
-    .when('/symbols', {
-        templateUrl: 'angular-app/symbols-display/symbols.html',
-        controller: SymbolsController,
-        controllerAs: 'vm',
-        access:{
-            restricted: false
-        }
-    })
-    .when('/symbol/:id', {
-        templateUrl: 'angular-app/stocks-display/stocks.html',
-        controller: StocksController,
-        controllerAs: 'vm',
-        access:{
-            restricted: false
-        }
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+    // .when('/', {
+    //     templateUrl: 'angular-app/main/main.html',
+    //     access:{
+    //         restricted: false
+    //     }
+    // })
+        .when('/', {
+            templateUrl: 'angular-app/symbols-display/symbols.html',
+            controller: SymbolsController,
+            controllerAs: 'vm',
+            access: {
+                restricted: false
+            }
+        })
+        .when('/symbol/:id', {
+            templateUrl: 'angular-app/stocks-display/stocks.html',
+            controller: StocksController,
+            controllerAs: 'vm',
+            access: {
+                restricted: false
+            }
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }
-
